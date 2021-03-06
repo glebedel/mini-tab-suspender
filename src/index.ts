49cc46c49d330ab93ab5560927a726935b764ef9
Guillaume Lebedel <guillaume.lebedel@yieldify.com>
2021-03-06 17:59:07 +0000

    simplify by only requiring refresh

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,20 @@
-  const TAB_TIMEOUT = 10800000; // 3 hours
-  const LOG_PREFIX = `Tampermonkey Tab Suspender: `;
-  const renderSuspendedElement = () =>
-    `<h1>Tab suspended. Click or press enter to restore the tab.</h1>`;
+const TAB_TIMEOUT = 10800000; // 3 hours
+const LOG_PREFIX = `Tampermonkey Tab Suspender: `;
+const renderSuspendedElement = () => `<h1>Tab suspended. Refresh to restore the tab.</h1>`;
 
-  console.log(`${LOG_PREFIX}script running`);
+console.log(`${LOG_PREFIX}script running`);
 
-  function bootstrapRestore() {
-    const LOG_PREFIX = `Tampermonkey Tab Suspender: `;
+function suspendTab() {
+  console.log(`${LOG_PREFIX}Suspend Tab!`);
+  window.location.href = 'about:blank';
+  document.write(`${renderSuspendedElement()}`);
+}
 
-    function restoreTab() {
-      console.log(`${LOG_PREFIX}restore previous tab`);
-      // window.history.back();
-      window.location.reload();
-    }
-    console.log(`${LOG_PREFIX}Tab suspended!`);
-    window.addEventListener('keypress', function (e) {
-      if (e.key === 'Enter') {
-        restoreTab();
-      }
-    });
-    window.addEventListener('click', restoreTab);
+let timeout: null | number = null;
+window.addEventListener('visibilitychange', () => {
+  if (document.hidden && navigator.mediaSession?.playbackState !== 'playing') {
+    timeout = setTimeout(suspendTab, TAB_TIMEOUT);
+  } else if (timeout !== null) {
+    clearTimeout(timeout);
   }
-
-  function suspendTab() {
-    console.log(`${LOG_PREFIX}Suspend Tab!`);
-    window.location.href = 'about:blank';
-    document.write(
-      `${renderSuspendedElement()}<script>${bootstrapRestore.toString()} bootstrapRestore();</script>`,
-    );
-  }
-
-  let timeout: null | number = null;
-  window.addEventListener('visibilitychange', () => {
-    if (document.hidden && navigator.mediaSession?.playbackState !== 'playing') {
-      timeout = setTimeout(suspendTab, TAB_TIMEOUT);
-    } else if (timeout !== null) {
-      clearTimeout(timeout);
-    }
-  });
+});
