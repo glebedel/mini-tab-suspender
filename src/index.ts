b91ccee9ccde563a476dad63d7c4cb844855a010
Guillaume Lebedel <guillaume.lebedel@yieldify.com>
2021-03-09 09:13:14 +0000

    more easily edit hours constant

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-const TAB_TIMEOUT = 10800000; // 3 hours
+const TAB_TIMEOUT_HOURS = 2;
+const HOURS_TO_MS = (hours: number) => hours * 60 * 60 * 1000;
+
 const LOG_PREFIX = `Tampermonkey Tab Suspender: `;
 const renderSuspendedElement = () => `<h1>Tab suspended. Refresh to restore the tab.</h1>`;
 
@@ -13,7 +15,7 @@ function suspendTab() {
 let timeout: null | number = null;
 window.addEventListener('visibilitychange', () => {
   if (document.hidden && navigator.mediaSession?.playbackState !== 'playing') {
-    timeout = setTimeout(suspendTab, TAB_TIMEOUT);
+    timeout = setTimeout(suspendTab, HOURS_TO_MS(TAB_TIMEOUT_HOURS));
   } else if (timeout !== null) {
     clearTimeout(timeout);
   }
