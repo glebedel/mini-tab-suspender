a0993a863c0ac523f6bf350da169d56aa6a98708
Guillaume Lebedel <guillaume.lebedel@yieldify.com>
2021-03-09 09:37:00 +0000

    chore(refactor): make code more readable

--- dist/index.js
+++ dist/index.js
@@ -2,22 +2,34 @@ const TAB_TIMEOUT_HOURS = 2;
 const HOURS_TO_MS = (hours) => hours * 60 * 60 * 1000;
 const LOG_PREFIX = `Tampermonkey Tab Suspender: `;
 const renderSuspendedElement = () => `<h1>Tab suspended. Refresh to restore the tab.</h1>`;
-console.log(`${LOG_PREFIX}script running`);
-function suspendTab() {
-    console.log(`${LOG_PREFIX}Suspend Tab!`);
-    window.location.href = 'about:blank';
-    document.write(`${renderSuspendedElement()}`);
-}
 let timeout = null;
+console.log(`${LOG_PREFIX}script running`);
 window.addEventListener('visibilitychange', () => {
-    var _a;
-    if (document.hidden && ((_a = navigator.mediaSession) === null || _a === void 0 ? void 0 : _a.playbackState) !== 'playing') {
-        timeout = setTimeout(suspendTab, HOURS_TO_MS(TAB_TIMEOUT_HOURS));
-    }
-    else if (timeout !== null) {
-        clearTimeout(timeout);
+    if (document.hidden) {
+        startSuspendTimer();
     }
 });
+function suspendTab() {
+    if (doNotSuspend()) {
+        console.log(`${LOG_PREFIX}Tab not suspendable. Restart Tab Suspension timer!`);
+        startSuspendTimer();
+    }
+    else {
+        console.log(`${LOG_PREFIX}Suspend Tab!`);
+        window.location.href = 'about:blank';
+        document.write(`${renderSuspendedElement()}`);
+    }
+}
+function startSuspendTimer() {
+    if (timeout !== null) {
+        clearTimeout(timeout);
+    }
+    console.log(`${LOG_PREFIX}Tab Suspension ${TAB_TIMEOUT_HOURS}h timer started!`);
+    timeout = setTimeout(suspendTab, HOURS_TO_MS(TAB_TIMEOUT_HOURS));
+}
+function doNotSuspend() {
+    return isMediaPlaying();
+}
 function isMediaPlaying() {
     var _a;
     const mediaElems = Array.prototype.slice.call(document.querySelectorAll('audio,video'));
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,53 @@ const TAB_TIMEOUT_HOURS = 2;
 const HOURS_TO_MS = (hours: number) => hours * 60 * 60 * 1000;
 
 const LOG_PREFIX = `Tampermonkey Tab Suspender: `;
+
+// HTML that will be written to the about:blank page when the tab is effectively suspended
 const renderSuspendedElement = () => `<h1>Tab suspended. Refresh to restore the tab.</h1>`;
+
+// Variable declared in scope to be able to clear the timeout after it's started
+let timeout: null | number = null;
 
 console.log(`${LOG_PREFIX}script running`);
 
-function suspendTab() {
-  console.log(`${LOG_PREFIX}Suspend Tab!`);
-  window.location.href = 'about:blank';
-  document.write(`${renderSuspendedElement()}`);
-}
-
-let timeout: null | number = null;
+// Start the suspension timer for the tab once it loses focus
 window.addEventListener('visibilitychange', () => {
-  if (document.hidden && navigator.mediaSession?.playbackState !== 'playing') {
-    timeout = setTimeout(suspendTab, HOURS_TO_MS(TAB_TIMEOUT_HOURS));
-  } else if (timeout !== null) {
-    clearTimeout(timeout);
+  if (document.hidden) {
+    startSuspendTimer();
   }
 });
 
+function suspendTab() {
+  if (doNotSuspend()) {
+    // if the tab should not be suspended for any reason we reset the timer.
+    console.log(`${LOG_PREFIX}Tab not suspendable. Restart Tab Suspension timer!`);
+    startSuspendTimer();
+  } else {
+    console.log(`${LOG_PREFIX}Suspend Tab!`);
+    window.location.href = 'about:blank';
+    document.write(`${renderSuspendedElement()}`);
+  }
+}
+
+function startSuspendTimer() {
+  if (timeout !== null) {
+    clearTimeout(timeout);
+  }
+  console.log(`${LOG_PREFIX}Tab Suspension ${TAB_TIMEOUT_HOURS}h timer started!`);
+  timeout = setTimeout(suspendTab, HOURS_TO_MS(TAB_TIMEOUT_HOURS));
+}
+
+/**
+ * @returns true if tab can be suspended
+ */
+function doNotSuspend() {
+  return isMediaPlaying();
+}
+
+/**
+ * 
+ * @returns whether or not an audio or video element is playing in tab
+ */
 function isMediaPlaying() {
   const mediaElems: HTMLMediaElement[] = Array.prototype.slice.call(document.querySelectorAll('audio,video'));
   const mediaElemPlaying = mediaElems.some((elem) => elem.duration > 0 && !elem.paused);
